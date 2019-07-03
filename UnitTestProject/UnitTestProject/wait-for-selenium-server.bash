f2efd317b806fe9ca3350dcd03e14537d66bded2
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2019-07-03 20:52:06 +0900

    modify option parameter

--- UnitTestProject/UnitTestProject/wait-for-selenium-server.bash
+++ UnitTestProject/UnitTestProject/wait-for-selenium-server.bash
@@ -7,7 +7,7 @@ for i in {1..10} ; do
 
 	if [ `curl http://$WEB_DRIVER_HOST:4444/  -o /dev/null -w '%{http_code}\n' -s` = 200 ]; then
 		#dotnet vstest $EXECUTE_TEST_MODULE >> /app/result/result.text
-		dotnet vstest $EXECUTE_TEST_MODULE --logger:trx;LogFileName=test.xml --ResultsDirectory:/app/result
+		dotnet vstest $EXECUTE_TEST_MODULE --logger:"trx;LogFileName=test.xml" --ResultsDirectory:/app/result
 		exit
 	fi
 	sleep 6
