ad5812f5d573987db1e188691ee65f49b3d0cbc1
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2019-07-03 20:39:31 +0900

    change vstest option

--- UnitTestProject/UnitTestProject/wait-for-selenium-server.bash
+++ UnitTestProject/UnitTestProject/wait-for-selenium-server.bash
@@ -3,9 +3,11 @@
 for i in {1..10} ; do
 
 	echo $i
+	echo test >> /app/result/result.text
 
 	if [ `curl http://$WEB_DRIVER_HOST:4444/  -o /dev/null -w '%{http_code}\n' -s` = 200 ]; then
-		dotnet vstest $EXECUTE_TEST_MODULE >> /app/result/result.text
+		#dotnet vstest $EXECUTE_TEST_MODULE >> /app/result/result.text
+		dotnet vstest $EXECUTE_TEST_MODULE --logger:trx;LogFileName=test.xml --ResultsDirectory:/app/result
 		exit
 	fi
 	sleep 6
