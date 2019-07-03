dbb970c3ea22c7c2c6b939a43dfac79e1b526c1c
きなこもちっち <kinakomotitti0210@yahoo.co.jp>
2019-07-03 21:13:20 +0900

    add wait process

--- UnitTestProject/UnitTestProject/wait-for-selenium-server.bash
+++ UnitTestProject/UnitTestProject/wait-for-selenium-server.bash
@@ -3,11 +3,16 @@
 for i in {1..10} ; do
 
 	echo $i
-	echo test >> /app/result/result.text
+	echo start >> /app/result/start.flag
 
 	if [ `curl http://$WEB_DRIVER_HOST:4444/  -o /dev/null -w '%{http_code}\n' -s` = 200 ]; then
-		#dotnet vstest $EXECUTE_TEST_MODULE >> /app/result/result.text
+		
+		echo start >> /app/result/test-start.flag
+		
 		dotnet vstest $EXECUTE_TEST_MODULE --logger:"trx;LogFileName=test.xml" --ResultsDirectory:/app/result
+		
+		echo start >> /app/result/test-end.flag
+		
 		exit
 	fi
 	sleep 6
